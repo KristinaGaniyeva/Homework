f1fb4ac9bbfcc21a3984e995a507584d1f31f4ab
Kristina Ganiyeva <ganiyevakris@gmail.com>
2018-10-16 20:33:48 +0600

    Formatted code

--- Homework1/src/it/sevenbits/homework/Main.java
+++ Homework1/src/it/sevenbits/homework/Main.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import static it.sevenbits.homework.parser.Parser.parse;
 
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         String str = "Hello How are you";
         System.out.println(Arrays.asList(parse(str)));
--- Homework1/src/it/sevenbits/homework/parser/Parser.java
+++ Homework1/src/it/sevenbits/homework/parser/Parser.java
@@ -2,26 +2,25 @@ package it.sevenbits.homework.parser;
 
 public class Parser {
 
-    public static String[] parse (String strInput){
+    public static String[] parse(String strInput) {
 
         int countSpace = 0;
 
         for (int i = 0; i < strInput.length(); i++) {
-            if (strInput.charAt(i) == ' '){
+            if (strInput.charAt(i) == ' ') {
                 countSpace++;
             }
         }
 
-        String mass[] = new String[countSpace+1];
+        String mass[] = new String[countSpace + 1];
 
         String tmpStr = "";
 
-        for(int i = 0, j = 0; i < strInput.length(); i++){
-            if(strInput.charAt(i) == ' '){
+        for (int i = 0, j = 0; i < strInput.length(); i++) {
+            if (strInput.charAt(i) == ' ') {
                 j++;
                 tmpStr = "";
-            }
-            else{
+            } else {
                 tmpStr = tmpStr + strInput.charAt(i);
             }
             mass[j] = tmpStr;
